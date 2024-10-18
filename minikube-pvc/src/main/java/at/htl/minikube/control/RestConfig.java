cf3916a118f8cce128d23680e8c68700e7b182c7
Armin1503 <arminsalkovic1503@gmail.com>
2024-10-18 12:08:06 +0200

    changed structure of project

--- minikube-pvc/src/main/java/at/htl/minikube/control/RestConfig.java
+++
@@ -1,8 +0,0 @@
-package at.htl.minikube.control;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class RestConfig extends Application {
-}
--- minikube-pvc/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO vehicle (brand, model) VALUES ('Opel', 'Kadett');
-INSERT INTO vehicle (brand, model) VALUES ('VW', 'Käfer 1400');
-INSERT INTO vehicle (brand, model) VALUES ('Opel', 'Blitz');
