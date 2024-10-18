c7f7431195b0e77584bf9673320b25baf0fb9685
Armin1503 <arminsalkovic1503@gmail.com>
2024-10-18 12:03:29 +0200

    added build.sh in root

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mvn -B package
-cp src/main/docker/Dockerfile target/
+cp ./src/main/docker/Dockerfile ./target/
 docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
 docker build --tag ghcr.io/armin1503/minikube-pvc/backend:latest ./target
 docker push ghcr.io/armin1503/minikube-pvc/backend:latest
